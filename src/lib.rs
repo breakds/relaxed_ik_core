b281e046a80515c4ea19c7f06a1fa9dd1f1afa35
Break Yang <breakds@gmail.com>
2024-06-30 15:29:01 -0700

    Support loading urdf from the same directory as the setting file

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,17 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn get_path_to_src() -> PyResult<String> {
+    Ok(utils_rust::file_utils::get_path_to_src())
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn relaxed_ik_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(get_path_to_src, m)?)?;
     Ok(())
 }
