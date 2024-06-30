9e9acf46e8b683bb17249dd3f5dd740bb377ed1e
Break Yang <breakds@gmail.com>
2024-06-30 16:18:00 -0700

    Implement solve for RelaxedIK python

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod relaxed_ik_web;
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 use numpy::{PyArray1, ToPyArray};
+use nalgebra::{Vector3, UnitQuaternion, Quaternion};
 
 #[pyclass]
 struct RelaxedIK {
@@ -23,7 +24,7 @@ impl RelaxedIK {
 
     // Note that the lifetime annotation `'py` ensures that the returned tuple
     // is correctly associated with the Python interpreter's lifetime.
-    
+
     #[getter]
     fn get_current_goal<'py>(&self, py: Python<'py>) -> PyResult<&'py PyTuple> {
         let position = self.inner.vars.goal_positions[0];
@@ -33,7 +34,20 @@ impl RelaxedIK {
         let quaternion_array = PyArray1::from_vec(py, vec![quaternion.w, quaternion.i, quaternion.j, quaternion.k]);
 
         Ok(PyTuple::new(py, &[position_array, quaternion_array]))
-    }    
+    }
+
+    pub fn solve<'py>(&mut self, py: Python<'py>, position: &PyArray1<f64>, quaternion: &PyArray1<f64>) -> &'py PyArray1<f64> {
+        let pos_slice = unsafe { position.as_slice().unwrap() };
+        let quat_slice = unsafe { quaternion.as_slice().unwrap() };
+
+        self.inner.vars.goal_positions[0] = Vector3::new(
+            pos_slice[0], pos_slice[1], pos_slice[2]);
+        self.inner.vars.goal_quats[0] = UnitQuaternion::from_quaternion(
+            Quaternion::new(quat_slice[0], quat_slice[1], quat_slice[2], quat_slice[3]));
+        
+        let result = self.inner.solve();
+        result.to_pyarray(py)
+    }
 }
 
 /// A Python module implemented in Rust. The name of this function must match
